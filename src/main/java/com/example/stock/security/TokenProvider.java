88cc738fe5433f194096e80a7064511054d5aada
kdmin0706 <gxxk76@gmail.com>
2023-10-27 19:50:14 +0900

    commit

--- src/main/java/com/example/stock/security/TokenProvider.java
+++ src/main/java/com/example/stock/security/TokenProvider.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
