cf11be03843e2453df1316cdb460336532c3467c
kdmin0706 <gxxk76@gmail.com>
2023-11-06 01:25:49 +0900

    commit

--- src/main/java/com/example/stock/security/SecurityConfiguration.java
+++ src/main/java/com/example/stock/security/SecurityConfiguration.java
@@ -14,6 +14,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
+@SuppressWarnings("deprecation")
 @Slf4j
 @Configuration
 @EnableWebSecurity
