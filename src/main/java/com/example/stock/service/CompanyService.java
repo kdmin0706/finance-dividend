cd82eb4f51625d297fa5abf126668558767423fc
kdmin0706 <gxxk76@gmail.com>
2023-11-06 01:13:11 +0900

    서비스(인터페이스,구현체) 분리

--- src/main/java/com/example/stock/service/CompanyService.java
+++ src/main/java/com/example/stock/service/CompanyService.java
@@ -1,23 +1,11 @@
 package com.example.stock.service;
 
-import com.example.stock.exception.impl.NoCompanyException;
 import com.example.stock.model.Company;
-import com.example.stock.model.ScrapedResult;
-import com.example.stock.persist.CompanyRepository;
-import com.example.stock.persist.DividendRepository;
 import com.example.stock.persist.entity.CompanyEntity;
-import com.example.stock.persist.entity.DividendEntity;
-import com.example.stock.scraper.Scraper;
-import lombok.AllArgsConstructor;
-import org.apache.commons.collections4.Trie;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.util.ObjectUtils;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 public interface CompanyService {
     Company save(String ticker);
--- src/main/java/com/example/stock/service/MemberService.java
+++ src/main/java/com/example/stock/service/MemberService.java
@@ -1,16 +1,9 @@
 package com.example.stock.service;
 
-import com.example.stock.exception.impl.AlreadyExistUserException;
 import com.example.stock.model.constant.Auth;
-import com.example.stock.persist.MemberRepository;
 import com.example.stock.persist.entity.MemberEntity;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 public interface MemberService {
     UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
